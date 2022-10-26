387a38613a5003a75821fc23a2d6c67713d20e87
IrenaDemchenkova <iren.demchenkova@gmail.com>
2022-10-26 15:54:30 +0300

    При делении на ноль функция возвращает исключение

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,12 @@ public class Calculator {
     BinaryOperator<Integer> plus = Integer::sum;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> y!=0 ? x / y : x/(y+1);
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y == 0) {
+            throw new ArithmeticException("Деление на ноль запрещено!");
+        }
+        return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
